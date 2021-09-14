f1f1bdc32dc50a0eb18c68309f093643fcb5bd42
Severin Mairinger <sevm@pm.me>
2021-09-14 20:51:24 +0200

    added mongodb util file

---
+++ backend/src/db/dbUtil.ts
@@ -0,0 +1,16 @@
+import {connect} from "mongoose";
+
+export function dbConnect(host: String, port: Number | String, authDatabase: String, username: String, password: String) {
+  return new Promise<string>(async (resolve, reject) => {
+    try {
+      await connect(`mongodb://${username}:${password}@${host}:${port}/?authSource=${authDatabase}&readPreference=primary`, {
+        // @ts-ignore
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+    } catch (error: any) {
+      reject(error);
+    }
+    resolve("Successfully connected to MongoDB Server!");
+  })
+}
