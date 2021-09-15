a5def55626a5aebbcd36452b6d12e0b02adb06b6
Severin Mairinger <mai.sevi@hotmail.com>
2021-09-15 18:26:09 +0200

    added oidc test handler

--- backend/src/routes/oAuthHandler.ts
+++ backend/src/routes/oAuthHandler.ts
@@ -7,7 +7,13 @@ const clients = [{
   redirect_uris: ['https://testapp/signin-oidc'],
   token_endpoint_auth_method: 'none'
 }];
-const oidc = new Provider(`https://localhost:${process.env.PORT}`);
+const oidc = new Provider(`https://localhost:${process.env.PORT}`, {
+  claims: {
+    email: ['email', 'email_verified'],
+    phone: ['phone_number', 'phone_number_verified'],
+    profile: ['birthdate', 'family_name', 'gender', 'given_name', 'given_name', 'locale', 'middle_name', 'name', 'nickname', 'picture', 'preferred_username', 'profile', 'updated_at', 'website', 'zoneinfo']
+  }
+});
 
 export function initProvider() {
   return new Promise(((resolve, reject) => {
