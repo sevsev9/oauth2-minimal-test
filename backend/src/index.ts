a33b646f8b2423b7e4eb10c49f1e8b8a4e026518
Severin Mairinger <mai.sevi@hotmail.com>
2021-09-15 18:27:14 +0200

    added axios and oauth routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,36 @@
 import express from "express";
 import dotenv from "dotenv";
 import {dbConnect} from "./db/dbUtil";
-import {initProvider} from "./routes/oAuthHandler";
-
+import {testFileRoute, oAuthReturnRoute, authGitHub} from "./routes/userRoutes";
+import axios from "axios";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.get('/test', testFileRoute);
+app.get('/oauth', oAuthReturnRoute);
+app.get('/auth', authGitHub);
+app.use('/', express.static('public'))
+
+app.get('/userdata', (req, res) => {
+
+  axios.get('https://api.github.com/user', {
+    headers: {
+      Authorization: 'token '+req.query.token
+    }
+  }).then(_res => {
+    res.status(200);
+    res.send(_res.data);
+    res.end();
+  }).catch(err => {
+    res.status(500);
+    res.send(err);
+    res.end();
+  });
+})
 
 dbConnect(
   process.env.MONGODB_HOST!,
@@ -18,12 +40,8 @@ dbConnect(
   process.env.MONGODB_PASSWORD!
 ).then(msg => {
   console.log(msg);
-  initProvider().then(oidc => {
-    //@ts-ignore
-    app.use('/', oidc.callback);
-    app.listen(process.env.PORT, () => {
-      console.log(`API running on port ${process.env.PORT}`);
-    });
+  app.listen(process.env.PORT, () => {
+    console.log(`API running on port ${process.env.PORT}`);
   });
 }).catch(err => {
   console.log(err);
