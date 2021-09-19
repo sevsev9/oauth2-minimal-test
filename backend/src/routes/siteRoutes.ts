38dd644c55f75fcb1f79a119bcf64cedb1dc1bec
Severin Mairinger <mai.sevi@hotmail.com>
2021-09-19 13:30:38 +0200

    added username check function

--- backend/src/routes/siteRoutes.ts
+++ backend/src/routes/siteRoutes.ts
@@ -1,7 +1,29 @@
 import {Request, Response} from "express";
 import path from "path";
+import {findUser} from "../db/dbUtil";
 
 export function testFileRoute(req: Request, res: Response) {
     res.status(200);
     res.sendFile(path.resolve("public/index.html"));
+}
+
+
+//@Todo secure endpoint of scraping attacks
+export function usernameExists(req: Request, res: Response) {
+    if (req.query.username) {
+        const usrname: string = req.query.username.toString();
+        findUser(usrname).then(usr => {
+            res.status(200);
+            res.send(usr.length !== 0);
+            res.end();
+        }).catch(err => {
+            res.status(500);
+            res.send(err);
+            res.end();
+        });
+    } else {
+        res.status(400);
+        res.send("Malformed request! Query has to include username.");
+        res.end();
+    }
 }
