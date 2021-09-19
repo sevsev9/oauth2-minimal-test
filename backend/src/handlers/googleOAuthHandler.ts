7c759994af735611884c92a3de35bdc80db82d91
Severin Mairinger <mai.sevi@hotmail.com>
2021-09-19 13:31:30 +0200

    added google oauth handler

---
+++ backend/src/handlers/googleOAuthHandler.ts
@@ -0,0 +1,81 @@
+import {OAuth2Client} from "google-auth-library";
+import {Request, Response} from "express";
+import {createMinimalUser, findUser} from "../db/dbUtil";
+import {MinimalUser} from "../types/MinimalUser";
+
+const url = 'https://people.googleapis.com/v1/people/me?personFields=names,emailAddresses,genders';
+let client: OAuth2Client;
+
+export function startAuth(req: Request, res: Response) {
+    client = new OAuth2Client(
+        process.env.GOOGLE_CLIENT_ID,
+        process.env.GOOGLE_SECRET,
+        'http://localhost:8080/google/callback'
+    );
+
+    const authUrl = client.generateAuthUrl({
+        access_type: 'offline',
+        scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email'
+    });
+
+    res.redirect(authUrl);
+}
+
+export function authCallback(req: Request, res: Response) {
+    const code = ""+req.query.code;
+
+    if (client) {
+        client.getToken(code).then(resp => {
+            client.setCredentials(resp.tokens);
+            console.log('Tokens acquired!');    //@Debug
+            res.redirect(`/google/userdata`); //@Todo fix redirect not working
+        }).catch(err => {
+            res.status(500);
+            res.send(err);
+            res.end();
+        })
+    } else res.redirect('/');
+
+}
+
+export function login(req: Request, res: Response) {
+    client.request({url}).then(resp => {
+        const name = {
+            // @ts-ignore
+            fname: resp.data.names.givenName,
+            // @ts-ignore
+            lname: resp.data.names.familyName
+        };
+        // @ts-ignore
+        const mail = resp.data.emailAddresses.value
+
+        async function getUname() {
+            let uname:string = ""+name.fname+name.lname
+            while (true) {
+                const res = await findUser(uname);
+                if (res) {
+                    uname+=Math.round(Math.random()*10);
+                } else {
+                    break;
+                }
+            }
+            return uname;
+        }
+
+        getUname().then(uname => {
+            //create user in database
+            createMinimalUser(new MinimalUser(mail, name.fname+name.lname, uname)).then(usr => {
+                console.log(usr); //@Debug
+                res.status(200);
+                //send user to screen where he can fill out additional info
+                res.send({
+                    name: name,
+                    mail: mail,
+                    username: uname
+                });
+                //res.redirect('/complete-login');
+                res.end();
+            })
+        });
+    });
+}
